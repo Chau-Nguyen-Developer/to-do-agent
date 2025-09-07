bbb004aee1009e8884ce5cad92c7d548e3279839
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2025-09-07 04:56:24 +0000

    chore: setting up the complete-task function. Basically work but need to review the logic. Have logic error.

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # to-do-agent
+
+## What I learned:
+1. When I add a task, I am only updating my Python variable in memory. At this point, the task is only in memory. When my program stops and if I do not save the changes back into .json file, the change will be lost. (aka the change is not reflected on my tasks.json file.)
+2. So, that means, I have to write it back (aka save the updated list) to tasks.json on disk. Now, if the program closes, the new task will still exist when I reload and run my program next time. 
+3. <b>Usually workflow:</b>
+    * Load tasks from tasks.json and save the data into a Python object. 
+    ```
+    def load_tasks():
+        try:
+            with open("tasks.json", 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                return data
+        except FileNotFoundError:
+            print("Error. File not found")
+        except json.JSONDecodeError:
+            print("Error. Could not decode JSON from file. Check if it is valid JSON file.")
+    ```
+    * Modify tasks by changing that Python object.
+    ```
+    add_task(myTask)
+    remove_task(task_number)
+    show_tasks()
+    ``` 
+    * Write/ dump that Python object back to tasks.json
+    ```
+        def save_tasks(myTask):
+            with open("tasks.json", "w", encoding="utf-8") as file:
+                json.dump(myTask, file, indent=4)
+                print("Finished saving tasks.")
+    ```
+4. load/loads and dump/dumps
+* json.load() -> read JSON directly from a file and convert it to Python objects (a dictionary/ a list)
+* json.loads() -> read JSON from a string and the rest are the same with above
+* json.dump() -> Take a Python object(ex: a list or dictionary) and write it as JSON to a file. 
+* json.dumps() -> converts Python object to a JSON string in memory (does not write to file). Have to write to file manually later. 
+
+```
+tasks = [
+    {"number: 1, "task": "Clean dishes", "done": False}
+]
+json_string=json.dumps(tasks, indent=4)
+
+with open("tasks.json", "w", encoding="utf-8") as file:
+    file.write(json_string)
+```
+Normally would use json.dump(). Only use json.dumps() when want to manipulate the JSON as string in memory first. 
+
+
+
--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import os
 def load_tasks():
     try:
         with open("tasks.json", 'r', encoding='utf-8') as file:
-            raw = file.read()
-            # print("Raw file content: ")
-            # print(raw)
-            data = json.loads(raw)
+            # raw = file.read()
+            # # print("Raw file content: ")
+            # # print(raw)
+            # data = json.loads(raw)
             # print(data)
+            data = json.load(file)
             return data
     except FileNotFoundError:
         print("Error. File not found")
@@ -19,18 +20,18 @@ def load_tasks():
 
 
 # save_tasks - writes to json
-def save_tasks(myTask):
+def save_tasks(my_task):
     with open("tasks.json", "w", encoding="utf-8") as file:
-        json.dump(myTask, file, indent=4)
-        print("Finished saving tasks.")
+        json.dump(my_task, file, indent=4)
+        print("JSON file got updated successfully.")
 
 #add_task(myTask) --> adds a task
-def add_task(myTask):
+def add_task(my_task):
     tasks = load_tasks() #1. Load existing
     new_number = len(tasks) + 1
-    tasks.append({"number": new_number, "task": myTask, "done": False})
+    tasks.append({"number": new_number, "task": my_task, "done": False})
     save_tasks(tasks)
-    print("Finished adding task: ", myTask)
+    print("Finished adding task: ", my_task)
 
 #remove_task(task_number) (function that helps remove task if accidently put the task in)
 def remove_task(task_number):
@@ -48,24 +49,37 @@ def remove_task(task_number):
 #show_tasks() --> prints tasks
 def show_tasks():
     tasks = load_tasks()
-    print(tasks)
+    if not tasks:
+        print("Currently have no tasks. Brain, let me give some tasks. What is a rational action now?")
+    else:
+        for t in tasks:
+            if t["done"]: 
+                status = "\u2713"
+            else:
+                status = "\u2717"
+            print(f"{t['number']}.{t['task']}[{status}]")
 
 #complete_task(task_number)-->mark a task as done 
 def complete_task(task_number):
     tasks = load_tasks()
+    tasks[task_number]["done"] = True;
+    save_tasks(tasks)
+    print("Completed task ", task_number)
 
 #main function
 def main():
     load_tasks()
     print("CURRENT TASKS")
     show_tasks()
-    print("ADD ONE MORE TASK")
-    add_task("Doing laundry.")
-    print("CURRENT TASKS AFTER ADDING")
-    show_tasks()
-    print("REMOVE TASK 1")
-    remove_task(1)
-    print("CURRENT TASKS ABOUT REMOVING TASK 1")
+    # print("ADD ONE MORE TASK")
+    # add_task("Clearing the table.")
+    # print("CURRENT TASKS AFTER ADDING")
+    # show_tasks()
+    # print("REMOVE TASK 2")
+    # remove_task(2)
+    # print("CURRENT TASKS ABOUT REMOVING TASK 1")
+    # show_tasks()
+    complete_task(3)
     show_tasks()
 
 
